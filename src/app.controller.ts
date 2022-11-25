88d72705a8321ae0a500cc52275824e368889a6e
RichardSaCa <caicedorich@unicauca.edu.co>
2022-11-25 16:01:47 -0600

    Actualizacion del archivo app.controller.ts para modelar una entidad Computador y verificar los verbos HTTP

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,94 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface Computador {
+  color: string,
+  marca: string,
+  ramGB: number
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-
-  private persona = "Mundo";
+  private computadores: Computador[] = [{
+    color: "negro",
+    marca: "hp",
+    ramGB: 12
+  }]
 
   @Get()
-  getHello(): string {
-    return `Hola: ${this.persona}`
+  getHello(): Computador[] {
+    return this.computadores;
   }
 
-  @Post(':nombre')
-  modificar(@Param('nombre') nombre: string): string {
-     this.persona = nombre;
-     return `Mensaje modificado: ${this.persona}`
+  @Post()
+  crear(@Body() datos: Computador): Computador {
+    this.computadores.push(datos);
+    return datos;
+  }
+
+  @Put(":id")
+  modificar(@Body() datos: Computador, @Param('id') id: number): Computador | string {
+    try {
+      this.computadores[id] = datos
+      return this.computadores[id];
+    }
+    catch {
+      return `No fue posible modificar el computador en la posición ${id}`
+    }
+  }
+
+  @Delete(":id")
+  eliminar(@Param('id') id: number) {
+    try {
+      this.computadores = this.computadores.filter((val, index) => index != id);
+      return true;
+    }
+    catch {
+      return false;
+    }
+  }
+
+  @Patch(":id/ramGB/:ramGB")
+  cambiarEdad(@Param('id') id: number, @Param('ramGB') ramGB: number): Computador | string {
+    try {
+      this.computadores[id].ramGB = ramGB;
+      return this.computadores[id];
+    }
+    catch {
+      return `No fue posible modificar al computador en la posición ${id}`
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
+// import { Controller, Get, Param, Post } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+
+//   private persona = "Mundo";
+
+//   @Get()
+//   getHello(): string {
+//     return `Hola: ${this.persona}`
+//   }
+
+//   @Post(':nombre')
+//   modificar(@Param('nombre') nombre: string): string {
+//      this.persona = nombre;
+//      return `Mensaje modificado: ${this.persona}`
+//   }
+// }
