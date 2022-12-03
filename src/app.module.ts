5dc7545f5b37ff00c8eae1b4863cd3f74be60836
RichardSaCa <caicedorich@unicauca.edu.co>
2022-12-02 20:52:57 -0600

    nueva rama para practica 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [  AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+       type: 'mongodb',
+       url: 'mongodb+srv://admin:<password>@cluster0.ico1ydr.mongodb.net/?retryWrites=true&w=majority',
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
